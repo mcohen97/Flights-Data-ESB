f4079e695a23d2ce862e6f12e123e00181cf0883
Marcel Cohen <diegomarcel1997@gmail.com>
2019-05-21 19:54:07 -0300

    se atrapan excepciones de autenticacion y se devuelven mensajes acorde

--- flight-data-mw/controllers/service-registry.js
+++ flight-data-mw/controllers/service-registry.js
@@ -7,12 +7,21 @@ module.exports = class ServiceRegistry{
 
     async register(req,res){
         let client = req.body;
-        console.log(client);
-        let token =await this.clients.add(client);
-        res.status(200);
-        res.json({
-            message: 'registration successful!',
-            token: token
-        });
+        client.responseContentType = req.accepts(['json','xml','text']);
+        let message;
+        try{
+            let token =await this.clients.add(client);
+            res.status(200);
+            message = {
+                message: 'registration successful!',
+                token: token
+            };
+        }catch(error){
+            res.status(400);
+            message ={
+                message: error.message
+            };
+        }
+        res.json(message);
     }
 }
