1958a68f7a8bd22e1bb3ac73a4af652c17fe73c9
Marcel Cohen <diegomarcel1997@gmail.com>
2019-05-21 19:54:44 -0300

    se cambian parametros de ServiceRegistry

--- flight-data-mw/controllers/router.js
+++ flight-data-mw/controllers/router.js
@@ -2,13 +2,16 @@ const express = require('express');
 const AirlinesClientsRepository = require('../repositories/repository')('airlines-clients');
 const ClientsCredentialsRepository = require('../repositories/repository')('clients-credentials');
 const AirlinesClientsService = require('../services/airlines-clients-service');
+const AuthenticationService = require('../services/authentication-service');
 const ServiceRegistry = require('./service-registry');
 const router = express.Router();
 
 const airlinesServicesRepository = new AirlinesClientsRepository();
 const clientsCredentialsRepository = new ClientsCredentialsRepository();
+const authService = new AuthenticationService(clientsCredentialsRepository,airlinesServicesRepository);
 
-const service = new AirlinesClientsService(airlinesServicesRepository, clientsCredentialsRepository);
+
+const service = new AirlinesClientsService(airlinesServicesRepository, authService);
 const clients = new ServiceRegistry(service);
 
 router.post('/register', (req, res) => clients.register(req,res));
