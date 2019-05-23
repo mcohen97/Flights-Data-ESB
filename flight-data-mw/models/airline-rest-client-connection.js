aee16e977fa9b6ab47eabfe24150a9578490acb8
Marcel Cohen <diegomarcel1997@gmail.com>
2019-05-23 20:00:56 -0300

    se manejan errores de envio de datos a clientes

--- flight-data-mw/models/airline-rest-client-connection.js
+++ flight-data-mw/models/airline-rest-client-connection.js
@@ -1,7 +1,7 @@
 const AirlineClient = require('./abstract-airline-client-connection');
 const Client = require('node-rest-client').Client;
 
-module.exports= class AirlineClientRest extends AirlineClient{
+module.exports= class AirlineRestClient extends AirlineClient{
 
     constructor(clientData){
         super(clientData);
@@ -9,11 +9,28 @@ module.exports= class AirlineClientRest extends AirlineClient{
         this.port = clientData.port;
     }
 
-    send(data){
+    async send(data){
         let client = new Client();
         let endpoint = `${this.url}:${this.port}`;
-        client.post(endpoint, data, function (received, response) {
-            //TODO: pending.
+        console.log("ENVIANDO...")
+        let req = client.post(endpoint, data, function (received, response) {
+            console.log(received);
+            console.log(response);
+        });
+
+        req.on('requestTimeout', function (req) {
+            console.log('request has expired');
+            req.abort();
+        });
+         
+        req.on('responseTimeout', function (res) {
+            console.log('response has expired');
+         
+        });
+         
+        //it's usefull to handle request errors to avoid, for example, socket hang up errors on request timeouts
+        req.on('error', function (err) {
+            console.log('request error');
         });
     }
 }
