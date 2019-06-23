bec5e16230d70dfe8ca23ed5ce8cc8b192c01707
Cantuccis <facuarancet97@gmail.com>
2019-06-23 17:12:33 -0300

    Se agrega model de Job. Un job es un dato que debe ser enviado a un cliente (tiene el dato y el cliente)

--- flight-data-mw/services/data-process-service.js
+++ flight-data-mw/services/data-process-service.js
@@ -1,4 +1,5 @@
 const DataFieldsTypes = require('../data-description/flight-data-fields-types');
+const Job = require('../models/job');
 const Logger = require('logger')('file');
 const logger = new Logger();
 
@@ -12,22 +13,16 @@ module.exports = class DataProcessService {
 
     async executeTriggers(dataReceived) {
         console.log(dataReceived.length);
-        let triggerCount = [];
         for (let data of dataReceived) {
             data = formatMessage(data);
             let connections = await this.clients.getByIata(data.AIRLINE);
-
             for(let connection of connections){
                 let trigger = connection.getTrigger();
                 if(trigger(data)){
-                    if(!triggerCount[data["AIRLINE"]])
-                        triggerCount[data["AIRLINE"]] = 0;
-                    triggerCount[data["AIRLINE"]]++;
                     filterAndValidate(this.filteringService, connection, data);
                 }
             }
         }
-        //logger.logInfo("AA times triggered: " + triggerCount["AA"]);
     }
 
     async send(job){
@@ -38,20 +33,8 @@ module.exports = class DataProcessService {
 }
 
 async function filterAndValidate(filteringService, client, data){
-    let job = {
-        message: data,
-        client: client,
-    };
-    /*let processedData =filteringService.applyTransformations(job);
-    processedData.then((result) => {
-                //console.log("procesado, resultado: ");
-                if(data["AIRLINE"] == "AA")
-                    logger.logInfo("AA DATA PROCESSED")
-                result.MW_CHECKOUT_TIMESTAMP = Date.now();
-                client.send(result);})
-                 .catch((err) => client.send({error: `${err.toString()} stacktrace: ${err.stack}`}));
-    */
-   filteringService.applyTransformations(job);
+    let job = new Job(data,client);
+    filteringService.applyTransformations(job);
 }
 
 function formatMessage(object) {
