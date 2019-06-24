78dc6f7493e503d94c091b726de33cf2bf2c4f69
= <diegomarcel27@hotmail.com>
2019-06-24 19:59:11 -0300

    se renombra paquete

--- FlightDataMW/business-logic/index.js
+++ FlightDataMW/business-logic/index.js
@@ -1,9 +1,9 @@
-module.exports.ServiceFactory = require('./models/airline-client-data-factory');
-module.exports.Abstract = require('./models/abstract-airline-client-data');
-module.exports.Rest = require('./models/airline-rest-client-data');
+module.exports.ServiceFactory = require('./entities/airline-client-data-factory');
+module.exports.Abstract = require('./entities/abstract-airline-client-data');
+module.exports.Rest = require('./entities/airline-rest-client-data');
 module.exports.EndpointTypes = require('./data-description/endpoint-types');
 module.exports.FlightDataFieldsList = require('./data-description/flight-data-fields').List;
 module.exports.FlightDataFieldsDictionary = require('./data-description/flight-data-fields').Dictionary;
-module.exports.TriggerParser = require('./models/trigger-parser');
+module.exports.TriggerParser = require('./entities/trigger-parser');
 module.exports.Airlines = require('./data-description/airlines');
 module.exports.DataFieldsTypes = require('./data-description/flight-data-fields-types');
