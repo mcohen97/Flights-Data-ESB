b7aee56b69aa9ac2fe7ed5fec679524721995a1a
= <diegomarcel27@hotmail.com>
2019-06-26 20:36:16 -0300

    se ajusta cliente

--- AirlinePlanning/servicio-de-planificacion-de-aerolinea/index.js
+++ AirlinePlanning/servicio-de-planificacion-de-aerolinea/index.js
@@ -62,9 +62,9 @@ var args = {
 };
 
 try{
-    client.post("http://localhost:6666/register", args, function (data, response) {
+    /*client.post("http://localhost:6666/register", args, function (data, response) {
         console.log(data);
-    });
+    });*/
      /*  client.put("http://localhost:6666/update/admin", args, function (data, response) {
         console.log(data);
     });*/
@@ -77,15 +77,15 @@ function testing(req){
     console.log("\n");
     console.log("----- PUBLICACION "+publication_number+" -----");
     console.log(req.body);
-    let mwTimeDifference = req.body.mw_checkout_timestamp - req.body.mw_checkin_timestamp;
-    let totalTime = Date.now() - req.body["publisher_checkout_timestamp"];
+    let mwTimeDifference = req.body["MW_CHECKOUT_TIMESTAMP"] - req.body["MW_CHECKIN_TIMESTAMP"];
+    let totalTime = Date.now() - req.body["PUBLISHER_CHECKOUT_TIMESTAMP"];
     mwTimeAccum += mwTimeDifference;
     totalTimeAccum += totalTime;
     mwAverage = mwTimeAccum/publication_number;
     totalAverage = totalTimeAccum/publication_number;
     if(first){
-        start = req.body.publisher_checkout_timestamp;
-        startMw = req.body.mw_checkin_timestamp;
+        start = req.body["PUBLISHER_CHECKOUT_TIMESTAMP"];
+        startMw = req.body["MW_CHECKIN_TIMESTAMP"];
         first = false;
     }
     end = Date.now();
